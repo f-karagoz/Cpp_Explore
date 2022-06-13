05ce8382512e2fb7dc7a24b8f27de44dea2aa643
Furkan Karagoz <fkaragoz14@gmail.com>
2022-06-13 23:35:31 +0300

    Test class added.

--- cpp_explore_1.cpp
+++ cpp_explore_1.cpp
@@ -2,10 +2,27 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
+
+class myClass
+{
+public:
+    int myInt;
+    char myChar;
+    string myString;
+};
 
 int main()
 {
+    myClass myObj;
+
+    myObj.myInt = 10;
+    myObj.myChar = 'A';
+    myObj.myString = "HELLO";
+
     std::cout << "Hello World!\n";
+    std::cout << "Int: " << myObj.myInt << "\tChar: " << myObj.myChar << "\tString: " << myObj.myString << "\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
@@ -18,3 +35,19 @@ int main()
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+/*  Steps Followed for Git
+* 
+*   1.  Git installed by Windows installer tool.
+*   2.  Git initialized for the current local repo by VS
+*   3.  Local directory navigated by PowerShell
+*   4.  Git add current .cpp file
+*   5.  Git commit -m "" commit message added
+*   6.  git remote add Cpp_Explore https://github.com/f-karagoz/Cpp_Explore.git
+*   7.  git push and voila!
+*
+*
+*
+*
+*
+*/
